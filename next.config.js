2cef01b0ea6bd66f4d0d7e51958dc1915f3a9aa4
hi-lottie <hi.lottie@icloud.com>
2022-06-08 00:37:42 +0200

    become typescript 🪄🧙🏻‍♀️

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   basePath: '',
   images: {
     domains: ['images.unsplash.com'],
